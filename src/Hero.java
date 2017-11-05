7040cd33f82694c125aa359443b842aed165cb1a
Nathan Luu <nateluu28@gmail.com>
2017-11-04 18:57:46 -0700

    I have created the basic form of Hero and some of its variables.

---
+++ src/Hero.java
@@ -0,0 +1,29 @@
+import acm.graphics.*;
+import acm.program.*;
+
+
+public class Hero {
+	
+	
+	private static final int HEIGHT = 10;
+	private static final int MAX_SPEED = 2;
+	private static final double ACCELERATION = .2;
+
+	
+	private int level;
+	private int HP; // health points
+	private int attack;
+	private int exp; //experience
+	private String name;
+	
+    // TO DO add?? the variables: entityType, knockback, inventory
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
