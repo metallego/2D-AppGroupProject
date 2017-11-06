48e12eae34e84f5ef740437deb151e83ccc45d5c
Jeremy Songster <j_songster@u.pacific.edu>
2017-11-06 13:29:44 -0800

    Added basic update() function to the Environment Class with Constants for environment variables

--- src/Environment.java
+++ src/Environment.java
@@ -4,5 +4,19 @@ import acm.program.*;
 
 public class Environment
 {
+    private Hero hero;
+    private double FRICTION = .4;
+    private double GRAVITY = .4;
 
+    public Environment(Hero h)
+    {
+        hero = h;
+    }
+    
+    private void update()
+    {
+        hero.move();
+        hero.applyFriction(FRICTION);
+        hero.applyGravity(GRAVITY);
+    }
 }
