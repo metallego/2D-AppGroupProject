f5f882e5979ee3aa7cc3ab8386d79ac816d607f7
metallego <atran5096@gmail.com>
2017-11-06 13:12:35 -0800

    option pane

---
+++ src/OptionsPane.java
@@ -0,0 +1,34 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.MouseEvent;
+
+import acm.graphics.GLabel;
+import acm.graphics.GObject;
+import acm.graphics.GOval;
+import acm.graphics.GRect;
+
+public class OptionsPane extends GraphicsPane{
+
+	/*
+	 * (non-Javadoc)
+	 * @see GraphicsPane#showContents()
+	 * ideas 
+	 * - volume toggle or slider? (toggle would probably be easier if we are being realistic)
+	 * - a way to change screen size?
+	 * - a way to toggle color blind mode? (extra most likely imo)
+	 * - a way to 
+	 */
+	
+	@Override
+	public void showContents() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hideContents() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
