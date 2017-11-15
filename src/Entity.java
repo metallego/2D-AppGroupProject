c69ebcc8967545f5319bb8d0f87c45096e078633
Nathan Luu <n_luu1@u.pacific.edu>
2017-11-15 12:58:57 -0800

    Added entity class

---
+++ src/Entity.java
@@ -0,0 +1,17 @@
+import acm.graphics.*;
+import acm.program.*;
+
+import java.awt.Color;
+import java.awt.geom.Point2D;
+
+import javax.imageio.ImageIO;
+
+
+public class Entity extends GraphicsProgram {
+	
+	private int HP; // health points
+	private int attack;
+	private String name;
+	
+	
+}
