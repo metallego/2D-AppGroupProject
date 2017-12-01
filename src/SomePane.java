418a2275f47a0f481909f0655bd522a87dce7d3c
Nathan Luu <n_luu1@u.pacific.edu>
2017-11-30 19:39:15 -0800

    fixed bugs on some pane

--- src/SomePane.java
+++ src/SomePane.java
@@ -10,46 +10,41 @@ public class SomePane extends GraphicsPane {
 	private GButton optionsButton; 
 	private InGameOptionsPane merp; 
 	private GImage background; 
-	
-	
+
+
 	public SomePane(MainApplication app) {
 		this.program = app;
 		exitButton = new GButton ("exit", 0, 0, 50, 50); 
 		optionsButton = new GButton ("Options", 0, 550, 50, 50); 
-		//img = new GImage("hero.jpg", 100, 100);
 		background = new GImage("background.jpg", 0, 0);
 		background.setSize(1080, 600);
 	}
-	
+
 	public void update() {
 		showContents();
 	}
-	
+
 	@Override
 	public void showContents() {
 		program.add(background);
 		program.add(program.hero.image);
-//		program.add(program.enemy.image);
-		//program.environment.setupPlatforms();
-		for( int i = 0; i < program.environment.getPlatforms().size(); i++)
-            program.add( program.environment.getPlatforms().get(i).getGImage() );
-		
+
 		for(Platform e:program.environment.getPlatforms()) {
 			GImage temp = e.getGImage();
 			program.add(temp);
 		}
 
-		
+
 		for(Loot e:program.environment.getLootList()) {
 			GImage temp = e.image;
 			program.add(temp);
 		}
-		
+
 		for(Entity e:program.environment.getEnemyList()) {
 			GImage temp = e.getImage();
 			program.add(temp);
 		}
-		
+
 		program.add(program.chest.image);
 		program.environment.setUpHeartSlots();
 		program.environment.setUpCoinSlots();
@@ -66,32 +61,46 @@ public class SomePane extends GraphicsPane {
 		program.remove(program.chest.image);
 		program.remove(exitButton);
 		program.remove(optionsButton);
-		
-		for( int i = 0; i < program.environment.getPlatforms().size(); i++)
-		    program.remove( program.environment.getPlatforms().get(i).getGImage() );
-		for (int i = 0; i < program.environment.getHeartSlotImage().size(); i++)
-			program.remove(program.environment.getHeartSlots().get(i).getGImage());	
-		for (int i = 0; i < program.environment.getCoinSlotImage().size(); i++)
-			program.remove(program.environment.getCoinSlots().get(i).getGImage());
-		for(int i = 0; i < program.environment.getEnemyList().size(); i++)
-			program.remove(program.environment.getEnemyList().get(i).image);
-	      for( int i = 0; i < program.environment.getLootList().size(); i++)
-	            program.remove( program.environment.getLootList().get(i).image );
-		
+
+		for(HeartSlots e:program.environment.getHeartSlots()) {
+			GImage temp = e.getGImage();
+			program.remove(temp);
+		}
+		for(CoinSlots e:program.environment.getCoinSlots()) {
+			GImage temp = e.getGImage();
+			program.remove(temp);
+		}
+
+		for(Platform e:program.environment.getPlatforms()) {
+			GImage temp = e.getGImage();
+			program.remove(temp);
+		}
+
+
+		for(Loot e:program.environment.getLootList()) {
+			GImage temp = e.image;
+			program.remove(temp);
+		}
+
+		for(Entity e:program.environment.getEnemyList()) {
+			GImage temp = e.getImage();
+			program.remove(temp);
+		}
+
 		program.environment.emptyLists();
 	}
-	
+
 	@Override
 	public void mousePressed(MouseEvent e) {
 		GObject obj = program.getElementAt(e.getX(), e.getY());
 		if(obj == program.hero.image || obj == exitButton) {
-		    program.environment.setPreserve( false );
-		    program.loadedLevel = false;
+			program.environment.setPreserve( false );
+			program.loadedLevel = false;
 			program.switchToMenu();
 		}
 		if(obj == optionsButton)
 		{
-		    program.environment.setPreserve( true );
+			program.environment.setPreserve( true );
 			program.switchtoIGOptions(); 
 		}
 	}
